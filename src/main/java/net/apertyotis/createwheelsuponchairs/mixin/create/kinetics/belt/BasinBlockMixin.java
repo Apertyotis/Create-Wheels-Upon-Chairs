fa760009caf1d55569f20e97bb4a3310db98569c
Apertyotis <1947517504@qq.com>
2026-07-09 14:26:40 +0800

    belt fix

---
+++ src/main/java/net/apertyotis/createwheelsuponchairs/mixin/create/kinetics/belt/BasinBlockMixin.java
@@ -0,0 +1,26 @@
+package net.apertyotis.createwheelsuponchairs.mixin.create.kinetics.belt;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.kinetics.belt.BeltBlockEntity;
+import com.simibubi.create.content.processing.basin.BasinBlock;
+import net.apertyotis.createwheelsuponchairs.AllConfig;
+import net.apertyotis.createwheelsuponchairs.content.belt.BeltBlockEntityEx;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = BasinBlock.class, remap = false)
+public abstract class BasinBlockMixin {
+    @WrapOperation(
+        method = "canOutputTo",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/simibubi/create/content/kinetics/belt/BeltBlockEntity;getSpeed()F"
+        )
+    )
+    private static float redirectGetSpeed(BeltBlockEntity instance, Operation<Float> original) {
+        if (!AllConfig.easy_belt)
+            return original.call(instance);
+        return ((BeltBlockEntityEx) instance).caa$getTargetSpeed();
+    }
+}
---
+++ src/main/java/net/apertyotis/createwheelsuponchairs/mixin/create/kinetics/belt/BrassTunnelBlockEntityMixin.java
@@ -0,0 +1,40 @@
+package net.apertyotis.createwheelsuponchairs.mixin.create.kinetics.belt;
+
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.simibubi.create.content.kinetics.belt.BeltBlockEntity;
+import com.simibubi.create.content.logistics.tunnel.BrassTunnelBlockEntity;
+import net.apertyotis.createwheelsuponchairs.AllConfig;
+import net.apertyotis.createwheelsuponchairs.content.belt.BeltBlockEntityEx;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = BrassTunnelBlockEntity.class, remap = false)
+public abstract class BrassTunnelBlockEntityMixin {
+    @WrapOperation(
+        method = "tick",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/simibubi/create/content/kinetics/belt/BeltBlockEntity;getSpeed()F"
+        )
+    )
+    private float redirectGetSpeed1(BeltBlockEntity instance, Operation<Float> original) {
+        if (!AllConfig.easy_belt)
+            return original.call(instance);
+        return ((BeltBlockEntityEx) instance).caa$getTargetSpeed();
+    }
+
+    @WrapOperation(
+        method = "addValidOutputsOf",
+        at = @At(
+            value = "INVOKE",
+            target = "Lcom/simibubi/create/content/kinetics/belt/BeltBlockEntity;getSpeed()F"
+        )
+    )
+    private float redirectGetSpeed2(BeltBlockEntity instance, Operation<Float> original) {
+        if (!AllConfig.easy_belt)
+            return original.call(instance);
+        return ((BeltBlockEntityEx) instance).caa$getTargetSpeed();
+    }
+}
